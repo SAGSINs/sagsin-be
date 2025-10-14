a1f9dd9ee011f0cb77028d726d5003e04ee0abcb
CulesBao <102230334@sv1.dut.udn.vn>
2025-10-14 21:47:48 +0700

    feat: refactor HeuristicModule imports; remove Mongoose integration and use forwardRef for Node and Links modules

--- src/heuristic/heuristic.module.ts
+++ src/heuristic/heuristic.module.ts
@@ -1,15 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { HeuristicService } from './heuristic.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { NodeEntity, NodeSchemaClass } from 'src/node/entities/node.entity';
-import { LinkEntity, LinkSchemaClass } from 'src/links/entities/link.entity';
+import { LinksModule } from 'src/links/links.module';
+import { NodeModule } from 'src/node/node.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: NodeSchemaClass.name, schema: NodeEntity },
-      { name: LinkSchemaClass.name, schema: LinkEntity },
-    ]),
+    forwardRef(() => NodeModule),
+    forwardRef(() => LinksModule),
   ],
   providers: [HeuristicService],
 })
